bca7eb3189a6fff28a8e9c82fbbb48d2891cbf67
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-05-22 00:09:28 -0300

    build: Cria o projeto com Java Mail Sender, RabbitMQ e Lombok

---
+++ src/main/java/com/notificacao/DsPedidosRabbitNotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsPedidosRabbitNotificacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsPedidosRabbitNotificacaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/notificacao/DsPedidosRabbitNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsPedidosRabbitNotificacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
